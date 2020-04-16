acafd45b3257978c237d87cdadbe4244b41c17e3
Joan Francisco Alvarado <joanfco1@gmail.com>
2020-04-15 21:56:11 -0500

    incluir connect constanst

---
+++ include/connect.h
@@ -0,0 +1,17 @@
+ 16 lines (14 sloc) 401 Bytes
+
+void connect(Stream &uart, WiFiClient &wifi, MQTTClient &client){
+  uart.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    uart.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect(MQTT_ID, MQTT_USER, MQTT_PASSWORD)) {
+    uart.print(".");
+    delay(1000);
+  }
+  uart.println("\nconnected!");
+  client.subscribe(String(MQTT_ID)+"/#");
+}
---
+++ include/constants.h
@@ -0,0 +1,8 @@
+#define SSID "Pixel"
+#define SSID_PASSWORD "3006750425"
+
+#define MQTT_ID "esp32_joan"
+#define MQTT_USER "Arduino"
+#define MQTT_PASSWORD "PASSWORD"
+
+#define BROKER "192.168.1.1"
