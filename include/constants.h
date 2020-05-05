80d2a5de5591ef1d586b6edc19bc66d211a44374
Joan Francisco Alvarado <joanfco1@gmail.com>
2020-05-05 09:57:52 -0500

    Se implementa giroscopio

--- include/constants.h
+++ include/constants.h
@@ -6,11 +6,18 @@
 #define MQTT_USER "Arduino"
 #define MQTT_PASSWORD "PASSWORD"
 
-#define BROKER "192.168.1.1"
+#define BROKER "192.168.1.3"
 #define REPORT_FREQ 20000
 #define MAX_ALARM_FREQ 5000
-
+#define TRIGMS 10
 ////////////////////////////
 
-
+#define MPU 0x68
+ 
+//Ratios de conversion
+#define A_R 16384.0 // 32768/2
+#define G_R 131.0 // 32768/250
+ 
+//Conversion de radianes a grados 180/PI
+#define RAD_A_DEG = 57.295779
 /////////////////////////
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <MQTT.h>
+#include <llp.h>
 
 #include <constants.h>
 #include <connect.h>
-
+#include <giro_sensor.h>
 
 WiFiClient net;
 MQTTClient client;
+GyroscopeSensor sensor = GyroscopeSensor(0x64, 1, 100);
 
 long reportTimer = millis();
 long alarmTimer = millis();
+long angulo = 0;
 
 void setup() {
   
@@ -19,20 +22,23 @@ void setup() {
   WiFi.begin(SSID, SSID_PASSWORD);
   client.begin(BROKER, net);
   connect(Serial, net, client);
+  sensor.init();
+  
 }
 
 void loop() {
   client.loop();
   delay(10);
-
+  angulo = sensor.read();
+  Serial.println(angulo);
   if (!client.connected()) {
     connect(Serial, net, client);
   }
 
   if(millis()-reportTimer >= REPORT_FREQ){
     reportTimer=millis();
-    client.publish(String(MQTT_ID)+"/report", String(30));
-    Serial.println(String(30));
+    client.publish(String(MQTT_ID)+"/report", String(angulo));
+    Serial.println(String(angulo));
   }
 
 
